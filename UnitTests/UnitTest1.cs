237f29f379d18c827aab68ca425552217e7c407f
Markus Flodmark <markus.flodmark@gmail.com>
2018-11-08 10:46:39 +0100

    unit test withdrawl cash

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -1,3 +1,6 @@
+using BankApp.Models;
+using BankApp.Repositories;
+using BankApp.ViewModels;
 using System;
 using Xunit;
 
@@ -5,10 +8,21 @@ namespace UnitTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(900, false)]
+        [InlineData(100, true)]
+        public void Withdrawl_Amount_ReturnTrueOrFalse(decimal amount, bool expected) 
         {
+            // arrange
+            var account = new Account() { Balance = 200 };
+            var vm = new BankViewModel() { Account = account, AmountToTransfer = amount };
+            var bank = new BankRepository();
+            
+            // act
+            var actual = bank.Withdrawl(vm);
 
+            // assert
+            Assert.Equal(expected, actual);
         }
     }
 }
